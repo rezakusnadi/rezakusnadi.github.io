b6f4feb0aaf1d000da9b85ca19008e4e3587408e
Reza Kusnadi <92531579+rezakusnadi@users.noreply.github.com>
2022-01-18 23:46:03 +0700

    Update app.js
    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
-const hamburgerButton = document.getElementById('hamburger')
-const navList = document.getElementById('nav-list')
+const backToTopButton = document.querySelector(".back-to-top");
+let isBackToTopRendered = false;
 
-function toggleButton()
-{
-    navList.classList.toggle('show')
-}
+let alterStyles = (isBackToTopRendered) => {
+  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+  backToTopButton.style.transform = isBackToTopRendered
+    ? "scale(1)"
+    : "scale(0)";
+};
 
-hamburgerButton.addEventListener('click',toggleButton)
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 700) {
+    isBackToTopRendered = true;
+    alterStyles(isBackToTopRendered);
+  } else {
+    isBackToTopRendered = false;
+    alterStyles(isBackToTopRendered);
+  }
+});
